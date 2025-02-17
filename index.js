57900f49ed7de5089355797548db32e9895d61bd
McCambley <jakemccamb16@gmail.com>
2025-02-16 22:47:24 -0500

    Create the game of life

---
+++ index.js
@@ -0,0 +1,87 @@
+/**
+ * @param {number[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var gameOfLife = function (board) {
+  // Store initial board in array
+  const initialState = JSON.parse(JSON.stringify(board));
+  // Iterate over the board and map over rows and update with function
+  board.forEach((row, rowIndex) => {
+    row.forEach((cell, columnIndex) => {
+      let current = cell;
+      let sumAll = 0;
+      let newVal = cell;
+      // Row above
+      const aboveRow = initialState[rowIndex - 1];
+      if (aboveRow) {
+        sumAll += aboveRow[columnIndex - 1] || 0;
+        sumAll += aboveRow[columnIndex] || 0;
+        sumAll += aboveRow[columnIndex + 1] || 0;
+      }
+      // Current row
+      sumAll += initialState[rowIndex][columnIndex - 1] || 0;
+      sumAll += initialState[rowIndex][columnIndex + 1] || 0;
+      const belowRow = initialState[rowIndex + 1];
+      if (belowRow) {
+        sumAll += belowRow[columnIndex - 1] || 0;
+        sumAll += belowRow[columnIndex] || 0;
+        sumAll += belowRow[columnIndex + 1] || 0;
+      }
+      // Assess newVal
+      if (current && (sumAll < 2 || 3 < sumAll)) {
+        newVal = 0;
+      }
+      if (current && (sumAll === 2 || sumAll === 3)) {
+        newVal = 1;
+      }
+      if (!current && sumAll === 3) {
+        newVal = 1;
+      }
+      // Assign newVal
+      board[rowIndex][columnIndex] = newVal;
+    });
+  });
+};
+
+const n = 3;
+const newBoard = Array.from({ length: n * 16 }, () => Array(n * 9).fill(0));
+newBoard.forEach((row, rowIndex) => {
+  row.forEach((column, columnIndex) => {
+    const value = Math.round(Math.random());
+    newBoard[rowIndex][columnIndex] = value;
+  });
+});
+
+function createTable(data) {
+  const table = document.createElement("table");
+  const tbody = document.createElement("tbody");
+
+  data.forEach((row) => {
+    const tr = document.createElement("tr");
+    row.forEach((num) => {
+      const td = document.createElement("td");
+      td.style.color = num
+        ? "oklch(39.53% 0.15 259.87)"
+        : "oklch(95.57% 0.003 286.35)";
+      td.style.backgroundColor = num
+        ? "oklch(70.94% 0.136 258.06)"
+        : "oklch(62.39% 0.181 258.33)";
+
+      td.textContent = "";
+      tr.appendChild(td);
+    });
+    tbody.appendChild(tr);
+  });
+  table.appendChild(tbody);
+  return table;
+}
+
+function updateBoardDisplay() {
+  document.body.textContent = "";
+  document.body.appendChild(createTable(newBoard));
+}
+
+setInterval(() => {
+  gameOfLife(newBoard);
+  updateBoardDisplay();
+}, (60 / 40) * 1000);
